35c8815878a6371d2eecccd366da0de76fda5637
RSN <rsn3230@gmail.com>
2020-08-08 16:18:22 +0530

    commit code for work on pwa

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,14 +1,33 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+
+// import { AppModule } from './app.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   imports: [
+//     AppModule,
+//     BrowserModule.withServerTransition({ appId: 'angular-starter' }),
+//     BrowserTransferStateModule
+//   ],
+//   bootstrap: [AppComponent],
+// })
+// export class AppBrowserModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     AppModule,
     BrowserModule.withServerTransition({ appId: 'angular-starter' }),
-    BrowserTransferStateModule
+    BrowserTransferStateModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   bootstrap: [AppComponent],
 })
