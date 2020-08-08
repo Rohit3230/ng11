9f8c9d11f952a37ccebc701c9ffe46244f71c7e2
RSN <rsn3230@gmail.com>
2020-08-08 17:23:08 +0530

    work for SEO

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -1,4 +1,27 @@
+// import { Component, OnInit } from '@angular/core';
+// import { environment } from '../../../../environments/environment';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
+
+//   name = environment.application.name;
+//   angular = environment.application.angular;
+//   bootstrap = environment.application.bootstrap;
+//   fontawesome = environment.application.bootstrap;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { environment } from '../../../../environments/environment';
 
 @Component({
@@ -11,11 +34,72 @@ export class HomeComponent implements OnInit {
   name = environment.application.name;
   angular = environment.application.angular;
   bootstrap = environment.application.bootstrap;
-  fontawesome = environment.application.bootstrap;
+  fontawesome = environment.application.fontawesome;
 
-  constructor() { }
+  features: any;
 
-  ngOnInit() {
+  constructor(
+    private meta: Meta,
+    private titleService: Title) {
+    this.features =
+      [
+        {
+          name: 'Bootstrap prototype',
+          description: 'Bootstrap Prototype Description',
+          icon: 'fab fa-bootstrap',
+          link: 'bootstrap-prototype'
+        },
+        {
+          name: 'Reactive Form',
+          description: 'Reactive Form Description',
+          icon: 'fab fa-bootstrap',
+          link: 'reactive-form'
+        },
+        {
+          name: 'Services',
+          description: 'Services Description',
+          icon: 'fab fa-bootstrap',
+          link: 'services'
+        },
+        {
+          name: 'Components',
+          description: 'Components Description',
+          icon: 'fab fa-bootstrap',
+          link: 'components'
+        },
+        {
+          name: 'Template Driven Forms',
+          description: 'Template Driven Forms Description',
+          icon: 'fab fa-bootstrap',
+          link: 'template-driven-forms'
+        },
+        {
+          name: 'Template Driven Forms',
+          description: 'Template Driven Forms Description',
+          icon: 'fab fa-bootstrap',
+          link: 'template-driven-forms'
+        },
+        {
+          name: 'Items',
+          description: 'Items',
+          icon: 'fab fa-bootstrap',
+          link: 'httpclient'
+        },
+      ];
+
   }
 
+  ngOnInit() {
+    this.titleService.setTitle('angular.ganatan: Une Application Web avec Angular');
+    this.meta.addTag({
+      name: 'angular.ganatan',
+      content: 'danny ganatan'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'Cette application a été développée avec Angular version 9.1.7 et bootstrap 4.5.0' +
+          ' Elle applique le Routing, le Lazy loading, le Server side rendering et les Progressive Web App (PWA)'
+      });
+  }
 }
