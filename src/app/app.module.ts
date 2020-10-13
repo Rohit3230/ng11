50c714a3bbc245379b9397bd76fcd8bf3656b491
RSN <rsn3230@gmail.com>
2020-10-13 17:01:28 +0530

    commit code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +24,7 @@ import { HttpService } from './services/http.services';
   //   HttpService
   // ],
   providers: [
-    HttpModule,
+    // HttpModule,
     UtilService,
     HttpService
   ],
